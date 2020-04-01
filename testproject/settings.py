704345d54fe24188d6bc74d97a2fe549973a9370
Hugues Michel <hug.michel@gmail.com>
2020-04-01 10:12:21 +0200

    init timetracker app

--- testproject/settings.py
+++ testproject/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'timetracker.apps.TimetrackerConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
